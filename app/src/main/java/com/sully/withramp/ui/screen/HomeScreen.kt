3cd43bcbc3d42b8f1ee0f287d83dcbb955b37c73
sullivan-shin <sullivan.shin@kakaocorp.com>
2025-11-06 08:52:30 +0900

    컴포넌트 예시

--- app/src/main/java/com/sully/withramp/ui/screen/HomeScreen.kt
+++ app/src/main/java/com/sully/withramp/ui/screen/HomeScreen.kt
@@ -1,18 +1,284 @@
 package com.sully.withramp.ui.screen
 
-import androidx.compose.foundation.layout.Box
+import android.widget.Toast
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.selection.selectableGroup
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.RadioButton
+import androidx.compose.material3.SegmentedButton
+import androidx.compose.material3.SegmentedButtonDefaults
+import androidx.compose.material3.SingleChoiceSegmentedButtonRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sully.withramp.ui.theme.WithRampTheme
 
 @Composable
 fun HomeScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center,
+    val context = LocalContext.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+            .padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(32.dp)
     ) {
-        Text(text = "Home")
+        Text(
+            text = "Home",
+            style = MaterialTheme.typography.titleLarge
+        )
+
+        Text(
+            text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis nisl ac lacus eleifend viverra. Ut mauris sem, egestas consectetur",
+            maxLines = 2,
+            overflow = TextOverflow.Ellipsis
+        )
+        
+        Button(
+            onClick = {
+                Toast.makeText(context, "Hello World", Toast.LENGTH_SHORT).show()
+            }
+        ) {
+            Text("say hello")
+        }
+
+        TextFieldExample()
+
+        PartialBottomSheet()
+
+        SingleChoiceSegmentedButton()
+
+        ElevatedCardExample()
+
+        CheckboxMinimalExample()
+
+        RadioButtonSingleSelection()
+    }
+
+}
+
+@Composable
+fun TextFieldExample() {
+    var userId by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    val context = LocalContext.current
+
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+    ) {
+        Column(
+            modifier =  Modifier.padding(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            TextField(
+                value = userId,
+                onValueChange = { text -> userId = text },
+                label = { Text("사용자 ID") }
+            )
+            TextField(
+                value = password,
+                onValueChange = { text -> password = text },
+                label = { Text("비밀번호") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password),
+                visualTransformation = PasswordVisualTransformation(),
+            )
+
+            Button(
+                enabled = userId.isNotEmpty() && password.isNotEmpty(),
+                onClick = {
+                    Toast.makeText(context, "$userId / $password", Toast.LENGTH_SHORT).show()
+                }
+            ) {
+                Text("login")
+            }
+
+        }
+    }
+
+
+}
+
+
+@Composable
+fun SingleChoiceSegmentedButton() {
+    var selectedIndex by remember { mutableIntStateOf(0) }
+    val options = listOf("Day", "Month", "Week")
+
+    SingleChoiceSegmentedButtonRow {
+        options.forEachIndexed { index, label ->
+            SegmentedButton(
+                shape = SegmentedButtonDefaults.itemShape(
+                    index = index,
+                    count = options.size
+                ),
+                onClick = { selectedIndex = index },
+                selected = index == selectedIndex,
+                label = { Text(label) }
+            )
+        }
+    }
+
+}
+
+@Composable
+fun ElevatedCardExample() {
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp),
+        ) {
+
+            Text(
+                text = "Elevated",
+            )
+            Text(
+                text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam quis nisl ac lacus eleifend viverra. Ut mauris sem, egestas consectetur",
+                maxLines = 2,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun PartialBottomSheet() {
+    var showBottomSheet by remember { mutableStateOf(false) }
+    val sheetState = rememberModalBottomSheetState(
+        skipPartiallyExpanded = false,
+    )
+
+    Column(
+        modifier = Modifier.fillMaxWidth(),
+    ) {
+        Button(
+            onClick = { showBottomSheet = true }
+        ) {
+            Text("Display partial bottom sheet")
+        }
+
+        if (showBottomSheet) {
+            ModalBottomSheet(
+                modifier = Modifier.fillMaxHeight(),
+                sheetState = sheetState,
+                onDismissRequest = { showBottomSheet = false }
+            ) {
+                Text(
+                    "Swipe up to open sheet. Swipe down to dismiss.",
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CheckboxMinimalExample() {
+    var checked by remember { mutableStateOf(true) }
+
+    Column(
+        modifier = Modifier.background(Color.Red.copy(alpha = 0.1f))
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Text(
+                "Minimal checkbox"
+            )
+            Checkbox(
+                checked = checked,
+                onCheckedChange = { checked = it }
+            )
+        }
+
+        Text(
+            if (checked) "Checkbox is checked" else "Checkbox is unchecked"
+        )
+    }
+}
+
+@Composable
+fun RadioButtonSingleSelection() {
+    val radioOptions = listOf("Calls", "Missed", "Friends")
+    var selectedOption by remember { mutableStateOf(radioOptions[0]) }
+
+    Column(
+        modifier = Modifier
+            .background(Color.Red.copy(alpha = 0.1f))
+            .selectableGroup()
+    ) {
+        radioOptions.forEach { text ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .selectable(
+                        selected = (text == selectedOption),
+                        onClick = { selectedOption = text },
+                    )
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (text == selectedOption),
+                    onClick = null
+                )
+                Text(
+                    text = text,
+                    style = MaterialTheme.typography.bodyLarge,
+                    modifier = Modifier.padding(start = 16.dp)
+                )
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+private fun HomeScreenPreview() {
+    WithRampTheme {
+        HomeScreen()
     }
 }
